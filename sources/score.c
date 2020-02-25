04cba241623f7e02e0d85143a96dd8cfc4f52e99
Mario miguel Pernia rosas <mpernia-@c1r11s5.42madrid.com>
2020-02-25 04:32:58 +0100

    holis

---
+++ sources/score.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   score.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpernia- <mpernia-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/25 04:18:53 by mpernia-          #+#    #+#             */
+/*   Updated: 2020/02/25 04:31:32 by mpernia-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/include_file.h"
+
+void	score_calculation(file *f)
+{
+	for(int i = 0; i < f->libraries; i++)
+	{
+		f->info_libraries[i].score = f->info_libraries[i].sum_score /
+											f->info_libraries[i].days_to_scan;
+	}
+}
