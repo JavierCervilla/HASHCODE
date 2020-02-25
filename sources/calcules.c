0a11bfc939a9790025537db8a45acd70f781eca6
Javier Cervilla miguel <jcervill@c1r11s3.42madrid.com>
2020-02-25 03:45:50 +0100

    tumierda

--- sources/calcules.c
+++ sources/calcules.c
@@ -6,7 +6,7 @@
 /*   By: jcervill <jcervill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/25 03:01:49 by jcervill          #+#    #+#             */
-/*   Updated: 2020/02/25 03:28:29 by jcervill         ###   ########.fr       */
+/*   Updated: 2020/02/25 03:41:32 by jcervill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,8 @@ void calculate_scores(file *f)
         while (j < f->info_libraries[i].nbooks)
         {
             f->info_libraries[i].sum_score = f->info_libraries[i].sum_score + (f->indiv_books[f->info_libraries[i].books_types[j]]);
+            printf("booktipe: %d\n", f->indiv_books[f->info_libraries[i].books_types[j]]);
+            printf("sum_score: %d\n", f->info_libraries[i].sum_score);
             j++;
         }
         i++;
