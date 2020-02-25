a575e9b2662319dc24ea307a47b9724b450dbe43
Javier Cervilla miguel <jcervill@c1r11s3.42madrid.com>
2020-02-25 04:06:54 +0100

    s

--- sources/calcules.c
+++ sources/calcules.c
@@ -6,7 +6,7 @@
 /*   By: jcervill <jcervill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/25 03:01:49 by jcervill          #+#    #+#             */
-/*   Updated: 2020/02/25 03:55:57 by jcervill         ###   ########.fr       */
+/*   Updated: 2020/02/25 04:05:12 by jcervill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ void calculate_scores(file *f)
 {
     int i = 0;
     int j = 0;
-    f->info_libraries[i].sum_score = 0;
     while (i < f->libraries)
     {
+        f->info_libraries[i].sum_score = 0;
         f->info_libraries[i].days_to_scan = ((f->info_libraries[i].nbooks / f->info_libraries[i].books_day) + f->info_libraries[i].ndays);
         //printf("sum_score: %d\n", f->info_libraries[i].sum_score);
         while (j < f->info_libraries[i].nbooks)
